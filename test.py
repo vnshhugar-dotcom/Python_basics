0d0762d0ab1e6ab052f462c178bd0b70c0b9e422
Surya Nandu <suryanandu11@gmail.com>
2026-06-25 16:56:46 +0530

    python basics

---
+++ test.py
@@ -0,0 +1,18 @@
+print("Hello World","World",sep="/")
+# print("hello world",end="")
+# print("hello world")
+x="25"
+# y="s"
+# print("abc">"abc")
+# x=int(input())
+print(int(float(x)))
+print("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+print("""hello
+      my name is
+      Avi
+      """)
+x="hello"
+print(x[0:3:2])
+print(x[::-1])
